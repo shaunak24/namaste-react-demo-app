afbdb853c2b592429505da948ed3ddf5f3cd7e4e
Shaunak <shaunak241997@gmail.com>
2023-01-01 13:18:50 +0530

    Functional components implementation

--- App.js
+++ App.js
@@ -11,18 +11,38 @@ import ReactDOM from 'react-dom/client';
 // JSX
 const nums = [1, 2, 3];
 
-const heading = (
-  <div id="container">
-    <h1 id="title" key="h1">
-      Hello from Shaunak
-    </h1>
-    <ul>
-      {nums.map((num) => (
-        <li key={`key-${num}`}>Element {num}</li>
-      ))}
-    </ul>
-  </div>
-);
+// const heading = (
+//   <div id="container">
+//     <h1 id="title" key="h1">
+//       Hello from Shaunak
+//     </h1>
+//     <ul>
+//       {nums.map((num) => (
+//         <li key={`key-${num}`}>Element {num}</li>
+//       ))}
+//     </ul>
+//   </div>
+// );
+
+// Functional component
+const Title = () => <h1>This is Title</h1>;
+
+const HeadingComponent = () => {
+  return (
+    <div id="container">
+      <Title />
+      <h2 id="sub-title" key="h2">
+        Hello from Shaunak
+      </h2>
+      <ul>
+        {nums.map((num) => (
+          <li key={`key-${num}`}>Element {num}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(heading);
+// root.render(heading);
+root.render(<HeadingComponent />);
