ce180d4d570f15c17e67e23c4de5ca7a05797805
Shaunak <shaunak241997@gmail.com>
2023-01-01 12:14:56 +0530

    Heading using JSX

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const heading = React.createElement(
-  'h1',
-  { id: 'heading', test: 'Shaunak' },
-  'Hello from Shaunak'
+// old way
+// const heading = React.createElement(
+//   'h1',
+//   { id: 'heading', test: 'Shaunak' },
+//   'Hello from Shaunak'
+// );
+
+// JSX
+const heading = (
+  <h1 id="title" key="h1">
+    Hello from Shaunak
+  </h1>
 );
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
