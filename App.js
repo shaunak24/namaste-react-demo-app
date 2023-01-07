78d11849faa66d83a293ac3ea76382b7f8c88572
Shaunak <shaunak241997@gmail.com>
2023-01-07 12:57:45 +0530

    Initial UI template for food ordering app

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css';
 
 // old way
 // const heading = React.createElement(
@@ -9,7 +10,7 @@ import ReactDOM from 'react-dom/client';
 // );
 
 // JSX
-const nums = [1, 2, 3];
+// const nums = [1, 2, 3];
 
 // const heading = (
 //   <div id="container">
@@ -25,24 +26,48 @@ const nums = [1, 2, 3];
 // );
 
 // Functional component
-const Title = () => <h1>This is Title</h1>;
+const Title = () => (
+  <img
+    className="logo"
+    alt="logo"
+    src="https://lh3.googleusercontent.com/Em7AHf7XBH_RtGfCBVXz9RH8SM_pHkj3xPP-yd3cRguY1_Jc8fmqgx6WxnvGVyPV5xs5gL3HCD0FCuv6Xo4CwoY6ak4"
+  />
+);
 
-const HeadingComponent = () => {
+const Header = () => {
   return (
-    <div id="container">
+    <div className="header">
       <Title />
-      <h2 id="sub-title" key="h2">
-        Hello from Shaunak
-      </h2>
-      <ul>
-        {nums.map((num) => (
-          <li key={`key-${num}`}>Element {num}</li>
-        ))}
-      </ul>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+          <li>Cart</li>
+        </ul>
+      </div>
     </div>
   );
 };
 
+const AppLayout = () => {
+  return (
+    <>
+      <Header />
+      <Body />
+      <Footer />
+    </>
+  );
+};
+
+const Body = () => {
+  return <h4>Body</h4>;
+};
+
+const Footer = () => {
+  return <h4>Footer</h4>;
+};
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 // root.render(heading);
-root.render(<HeadingComponent />);
+root.render(<AppLayout />);
