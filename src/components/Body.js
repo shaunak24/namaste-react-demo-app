142c731bbd97fa8c3bdf87e190fb0d66b51a981f
Shaunak <shaunak241997@gmail.com>
2023-01-08 13:47:20 +0530

    Implement search functionality

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,10 +1,8 @@
 import RestaurantCardList from './RestaurantCardList';
-import SearchBar from './SearchBar';
 
 const Body = () => {
   return (
     <>
-      <SearchBar />
       <RestaurantCardList />
     </>
   );
--- src/components/RestaurantCardList.js
+++ src/components/RestaurantCardList.js
@@ -1,13 +1,47 @@
 import RestaurantCard from './RestaurantCard';
 import restaurantsData from '../../resources/restaurants.json';
+import { useState } from 'react';
 
 const RestaurantCardList = () => {
+  const [searchText, setSearchText] = useState();
+  const [restaurants, setRestaurants] = useState(restaurantsData.data);
+
   return (
-    <div className="restaurant-list">
-      {restaurantsData.data.map((res) => (
-        <RestaurantCard {...res.data} key={res.data.id} />
-      ))}
-    </div>
+    <>
+      <div className="search-wrap">
+        <div className="search-container">
+          <input
+            className="search-input"
+            type="text"
+            placeholder="Search"
+            value={searchText}
+            onChange={(e) => setSearchText(e.target.value)}
+          />
+          <button
+            className="search-btn"
+            onClick={() => {
+              if (!searchText) {
+                setRestaurants(restaurantsData.data);
+                return;
+              }
+              // filter data (always on original list)
+              const data = restaurantsData.data.filter((res) =>
+                res.data.name.toUpperCase().includes(searchText.toUpperCase())
+              );
+              // update the state - restaurants
+              setRestaurants(data);
+            }}
+          >
+            <i className="fa fa-search"></i>
+          </button>
+        </div>
+      </div>
+      <div className="restaurant-list">
+        {restaurants.map((res) => (
+          <RestaurantCard {...res.data} key={res.data.id} />
+        ))}
+      </div>
+    </>
   );
 };
 
