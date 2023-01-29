a8e827cd4f968c0e4edf116441f3895d3b8f27d7
Shaunak <shaunak241997@gmail.com>
2023-01-30 01:08:07 +0530

    Fix Shimmer

--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -2,11 +2,14 @@ import React from 'react';
 
 const Shimmer = () => {
   return (
-    <div className="flex flex-wrap p-5 bg-cyan-200 my-5">
+    <div className="flex flex-wrap p-5 my-5">
       {Array(15)
         .fill('')
         .map((e, index) => (
-          <div key={index} className="w-56 m-2 p-2 shadow-lg bg-cyan-200"></div>
+          <div
+            key={index}
+            className="w-56 h-56 m-2 p-2 shadow-lg bg-cyan-200"
+          ></div>
         ))}
     </div>
   );
