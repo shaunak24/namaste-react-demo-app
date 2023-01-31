8a812fb502e773519088e5947e2626bc1c42a226
Shaunak <shaunak241997@gmail.com>
2023-01-31 22:55:55 +0530

    Minor UI enhancement

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -10,25 +10,35 @@ const RestaurantCard = ({
   address,
   avgRating,
 }) => {
+  const ratingBackgroundStyle = getRatingBackgroundStyle(avgRating);
+
+  function getRatingBackgroundStyle(rating) {
+    if (rating >= 4) return 'bg-green-600';
+    else if (rating >= 3 && rating < 4) return 'bg-orange-300';
+    else return 'bg-red-600';
+  }
+
   return (
-    <div className="w-56 m-2 p-2 shadow-lg bg-cyan-200">
+    <div className="w-56 h-72 m-2 p-2 shadow-lg bg-cyan-200">
       <img
         src={`${SWIGGY_IMAGE_CDN_URL}${cloudinaryImageId}`}
         alt="Restaurant Logo"
       />
-      <h2 className="font-bold text-xl">{name.toUpperCase()}</h2>
+      <h4 className="font-bold">{name.toUpperCase()}</h4>
       <h4>{area}</h4>
-      <h4>{cuisines.join(', ')}</h4>
-      <div className="flex">
-        <p className="text-sm px-2">{`${
-          avgRating === '--' ? 0 : avgRating
-        } ⭐️`}</p>
+      <h4 className="text-xs my-2">{cuisines.join(', ')}</h4>
+      <div className="flex text-xs">
+        <div className={`mx-2 w-16 h-4 rounded-md ${ratingBackgroundStyle}`}>
+          <p className="text-center">{`${
+            avgRating === '--' ? 0 : avgRating
+          } ⭐️`}</p>
+        </div>
         <p>|</p>
-        <p className="text-sm px-2">{`${Math.round(lastMileTravel)} Kms`}</p>
+        <p className="px-2">{`${Math.round(lastMileTravel)} Kms`}</p>
         <p>|</p>
-        <p className="text-sm px-2">{costForTwoString}</p>
+        <p className="px-2">{costForTwoString}</p>
       </div>
-      <p>{`📭 ${address}`}</p>
+      {/* <p>{`📭 ${address}`}</p> */}
     </div>
   );
 };
