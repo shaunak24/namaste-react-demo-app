332be76ab29513977a4f0f1ec64892993aca82fd
Shaunak <shaunak241997@gmail.com>
2023-02-03 23:52:08 +0530

    Update context name for react dev tools

--- src/utils/UserContext.js
+++ src/utils/UserContext.js
@@ -1,8 +1,12 @@
 import { createContext } from 'react';
 
-export default UserContext = createContext({
+const UserContext = createContext({
   user: {
     name: 'Dummy',
     email: 'dummy@gmail.com',
   },
 });
+
+// display name for debugging using react dev tools
+UserContext.displayName = 'UserContext';
+export default UserContext;
