da0fe339b7d37982864e3b4d19bf3efe409d9cc4
Shaunak <shaunak241997@gmail.com>
2023-02-03 22:49:33 +0530

    Create user context in class based components

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
+import UserContext from '../utils/UserContext';
 import Profile from './Profile';
 
 export class About extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    // place to make API calls
+  }
+
   render() {
     return (
       <div>
         <h1>About Us Page</h1>
-        <p>Hello From Shaunak!</p>
+
+        <UserContext.Consumer>
+          {({ user }) => <p>Hello From {user.name}!</p>}
+        </UserContext.Consumer>
         <Profile />
       </div>
     );
